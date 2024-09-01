7b4afdee4c621755e937cbb1fd500e25a4dab979
WhiteDevil1097 <techaccess1096@gmail.com>
2024-09-01 10:38:40 +0530

    Add files via upload

---
+++ @Code_Breaker.py
@@ -0,0 +1,15 @@
+# GET HTML of webpage               
+import requests as re 
+
+response = re.get('https://www.smartvalue.biz')
+html = response.text
+
+print(html)                             
+(logo) = '''
+ _  _  ____  ____     ___  __  ____  ____
+/ )( \(  __)(  _ \   / __)/  \(    \(  __)            \ /\ / ) _)  ) _ ( _( (__(  O )) D ( ) _)
+(_/\_)(____)(____/(_)\___)\__/(____/(____)
+'''
+print(logo)
+print('      »»»Devoloper By White_Devil«««')
+print('     _________________________________')
